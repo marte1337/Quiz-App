34e84959aa832f9be0f13c69c6f36eb2972e6272
Marte Auer <marte_auer@hotmail.com>
2023-03-08 10:10:05 +0100

    add button js WIP

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
-const darkElements = document.querySelector('[data-js="dark"]')
+// DARK MODE TOGGLE
 
-const btnToggle = document.querySelector('[data-js="dark-mode-button"]')
+const darkElements = document.querySelector('[data-js="dark"]')
+const btnToggle = document.querySelector('[data-js="dark-mode-switch"]')
 
 btnToggle.addEventListener("click", () => {
     console.log("click")
     darkElements.classList.toggle("dark-mode")
+})
+
+
+// ANSWER CARD
+
+const cardAnswer = document.querySelector('[data-js="answer-card"]')
+const btnAnswer = document.querySelector('[data-js="answer-button"]')
+
+console.log(cardAnswer, btnAnswer)
+
+
+btnAnswer.addEventListener("click", () => {
+    console.log("click")
+    cardAnswer.classList.remove("hidden")
 })
